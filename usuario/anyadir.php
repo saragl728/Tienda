0ede2ed7c5cda960b1bfc623d13f5b52de85887d
saragl728 <saragl728@gmail.com>
2025-04-02 14:39:41 +0200

    Creo un método para añadir un usuario

---
+++ usuario/anyadir.php
@@ -0,0 +1,32 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+$json = file_get_contents('php://input');
+
+$params = json_decode($json);
+
+require("../conexion.php");
+require("../comun.php");
+$con = retornarConexion();
+
+$noombre = limpiaEstring($params->nombre);
+$correoo = limpiaEstring($params->correo);
+$fechaNaac = $params->$fechaNac;
+$passwd = password_hash($params->contrasenya, PASSWORD_DEFAULT);
+$fechaNaac = date("Y-m-d", strtotime($fechaNaac));
+
+//no tengo que poner el rol o el saldo porque tienen valores por defecto
+mysqli_query($con,"insert into usuario(nombre,correo,fechaNac,contrasenya) values ('$noombre','$correoo', '$fechaNaac', '$passwd')");
+
+class Result
+{
+}
+
+$response = new Result();
+$response->resultado = 'OK';
+$response->mensaje = 'datos grabados';
+
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
