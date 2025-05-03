bd3d236304cf0051d727a94f604e957d33e6b145
saragl728 <saragl728@gmail.com>
2025-05-03 20:47:09 +0200

    Modifico una función para que quite ciertas palabras prohibidas

--- comun.php
+++ comun.php
@@ -2,8 +2,21 @@
 //función que limpia un string
 function limpiaEstring($cadena): string
 {
+    $arrayProhibido = ['cabrón', 'gilipollas', 'puto', 'subnormal', 'travelo']; //array con palabras prohibidas
+    $patron = '/';
+
+    for ($i = 0; $i < count($arrayProhibido); $i++){
+        if ($i < count($arrayProhibido) - 1){
+            $patron .= "$arrayProhibido[$i]|";
+        }
+        else{
+            $patron .= "$arrayProhibido[$i]/i";
+        }
+    }
+
     $salida = trim($cadena);
     $salida = stripslashes($salida);
+    $salida = preg_replace($patron, '?', $salida);
     $salida = htmlspecialchars($salida);
     return $salida;
 }
