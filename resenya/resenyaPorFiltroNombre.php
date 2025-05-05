42555bae5a435272eaa4ac0f8bc4f0b5e6d843c5
saragl728 <saragl728@gmail.com>
2025-05-05 09:43:04 +0200

    Hago que se pueda buscar reseñas por el nombre del producto

---
+++ resenya/resenyaPorFiltroNombre.php
@@ -0,0 +1,17 @@
+<?php
+  require("../header.php");
+
+  require("../conexion.php");
+  $con = retornarConexion();
+
+  $registros = mysqli_query($con, "SELECT resenya.Id AS Id, usuario.nombre AS cliente, producto.nombre AS producto, resenya.contenido AS opinion, resenya.fecha AS fecha FROM usuario
+  INNER JOIN resenya ON usuario.Id = resenya.IdCliente INNER JOIN producto ON resenya.IdProducto = producto.Id WHERE producto.nombre LIKE '%$_GET[nombre]%'");
+  $vec = [];
+  while ($reg = mysqli_fetch_array($registros)) {
+    $vec[] = $reg;
+  }
+
+  $cad = json_encode($vec);
+  echo $cad;
+  header('Content-Type: application/json');
+?>
