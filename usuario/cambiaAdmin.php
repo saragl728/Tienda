4a569e30a4ed18477fae977b21c4d69b931a3a89
saragl728 <saragl728@gmail.com>
2025-05-05 11:17:15 +0200

    Añado funciones para dar y quitar permisos de administrador

---
+++ usuario/cambiaAdmin.php
@@ -0,0 +1,22 @@
+<?php 
+  require("../header.php");
+  
+  $json = file_get_contents('php://input');
+ 
+  $params = json_decode($json);
+  
+  require("../conexion.php");
+  require("../comun.php");
+  $con=retornarConexion();
+  
+  mysqli_query($con,"update usuario set adminis='$params->adminis' where Id=$params->Id");
+     
+  class Result {}
+
+  $response = new Result();
+  $response->resultado = 'OK';
+  $response->mensaje = 'datos modificados';
+
+  header('Content-Type: application/json');
+  echo json_encode($response);  
+?>
---
+++ usuario/sacarTodosMenosUsuario.php
@@ -0,0 +1,18 @@
+<?php 
+  require("../header.php");
+  
+  require("../conexion.php");
+  $con=retornarConexion();
+
+  $registros=mysqli_query($con,"select Id, nombre, correo, fechaNac, saldo, contrasenya, adminis from usuario WHERE Id != $_GET[Id]");
+    
+  $vec=[];  
+  while ($reg=mysqli_fetch_array($registros))  
+  {
+    $vec[]=$reg;
+  }
+  
+  $cad=json_encode($vec);
+  echo $cad;
+  header('Content-Type: application/json');
+?>
