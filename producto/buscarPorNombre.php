9aca9ab6b591393384d160c4c03e48479e264c9b
saragl728 <saragl728@gmail.com>
2025-05-05 08:52:16 +0200

    Añado una función para buscar por nombre

---
+++ producto/buscarPorNombre.php
@@ -0,0 +1,18 @@
+<?php 
+  header('Access-Control-Allow-Origin: *'); 
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  
+  require("../conexion.php");
+  $con=retornarConexion();
+
+  $registros=mysqli_query($con,"select Id, nombre, precio from producto WHERE nombre LIKE '%$_GET[nombre]%'");
+  $vec=[];  
+  while ($reg=mysqli_fetch_array($registros))  
+  {
+    $vec[]=$reg;
+  }
+  
+  $cad=json_encode($vec);
+  echo $cad;
+  header('Content-Type: application/json');
+?>
