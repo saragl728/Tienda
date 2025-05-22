5266d34839a11bb7ade6950c2f5a06d15aa0a2ca
saragl728 <saragl728@gmail.com>
2025-05-22 14:42:22 +0200

    Añado funciones que sacan el nº de administradores y de usuarios

---
+++ usuario/numeroUsuarios.php
@@ -0,0 +1,8 @@
+<?php 
+  require "../req/header.php";
+  require "../req/conexion.php";
+  $con=retornarConexion();
+
+  $registros=mysqli_query($con,"select COUNT(*) AS 'cantidad' from usuario where Id <> 0");
+  require "../req/buscaUno.php";
+?>
